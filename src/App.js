9b93d433050bdd3d92775edc048a67037be16411
amwa <amy@Amys-MacBook-Pro-2.local>
2018-06-28 12:12:17 -0400

    added navigation, images, people

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
-import BackgroundImage from './img1.jpg';
 
 import './App.css';
+import Carousel from './carousel.js'
 
 const { Header, Footer, Sider, Content } = Layout;
 
 
 class App extends Component {
+
   render() {
     return (
       <Layout>
@@ -56,10 +57,11 @@ class App extends Component {
           An ELVO, or “emergent large vessel occlusion,” is a particularly severe category of stroke
           whereby one of the major blood vessels feeding the brain is blocked. We use deep learning to
           improve a radiologist’s workflow and prevent unnecessary
-          disruptions by the stroke team waiting for rapid interpretation. 
-          </p>
-          <p> <img src={require('./img1.jpg')} alt='Examples of ELVO CT scans' /> </p>
-          <p> Several CT scan ELVO examples. </p>
+          disruptions by the stroke team waiting for rapid interpretation.  </p>
+          <p> Examples of ELVO CTAs: </p>
+        </Content>
+        <Content>
+          <div class="container"> {<Carousel />} </div>
         </Content>
         <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 200 }}>
           <h3 id="people"> People</h3>
@@ -80,6 +82,7 @@ class App extends Component {
         </Content>
         <Content style={{ padding: 24, margin: 0, minHeight: 200 }}>
           <h3 id="updates">Updates</h3>
+          <p> (June 2018) 90% validation accuracy achieved on 977 CTA images </p>
           <p>Group selected as a recipient of a Karen T. Romer Award</p>
           <p><a href="https://www.brown.edu/academics/college/fellowships/utra/sites/brown.edu.academics.college.fellowships.utra/files/uploads/Medical%20Imaging%20Data%20Analytics%20with%20Deep%20Learning-Ugur%20Cetintemel%20and%20Ryan%20McTaggart.pdf">
             Summer 2018 I-UTRA Proposal Released
